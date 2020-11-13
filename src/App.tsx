52e35fb52c0045b5005b8f538396f51bb56a9fa0
Marcin Kowal <marcin.kowal@medius.com>
2020-11-13 11:00:41 +0100

    Router introduced

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,67 @@ import React from 'react';
 import AppBar from "./components/AppBar";
 import Result from "./components/Result/Result";
 import BottomNavigation from "./components/BottomNavigation";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TestComponent from "./components/Test";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        app: {
-          //textAlign: "center",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column"
-        },
-        content: {
-          flex: 1
-        }
-    }),
+  createStyles({
+    app: {
+      //textAlign: "center",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column"
+    },
+    content: {
+      flex: 1
+    }
+  }),
 );
 
 function App() {
   const classes = useStyles();
   return (
-    <div className={classes.app}>
-      <AppBar />
-      <div className={classes.content}>
-        <TestComponent/>
-        <Result />
+    <Router>
+      <div className={classes.app}>
+        <Switch>
+          <Route exact path="/">
+            <>
+            Nothing yet here, check paths:
+            /test
+            /result
+            </>
+          </Route>
+          <Route path="/test">
+            <>
+              <AppBar />
+              <div className={classes.content}>
+                <TestComponent />
+              </div>
+              <BottomNavigation />
+            </>
+
+          </Route>
+          <Route path="/result">
+            <>
+              <AppBar />
+              <div className={classes.content}>
+                <Result />
+              </div>
+              <BottomNavigation />
+            </>
+
+          </Route>
+        </Switch>
+
       </div>
-      <BottomNavigation />
-    </div>
+    </Router>
+
   );
 }
 
