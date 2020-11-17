8ad592505afe54ca6a470f5f2e9257a5c880fae8
grzegorzb <grzegorzb@medius-group.com>
2020-11-17 17:10:05 +0100

    Fixed build

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TwitterIcon from '@material-ui/icons/Twitter';
-import { Typography, IconButton } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import EmailIcon from '@material-ui/icons/Email';
 import WhatsappIcon from '@material-ui/icons/WhatsApp';
